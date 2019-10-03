f8a54a86d38a4a2f14ed08eb6886cf4d6f7ddf77
yhatanaka0804 <yhatanaka0804@gmail.com>
2019-10-03 18:46:29 +0900

    [WIP]feat:ストレージ保存

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'edit', loadChildren: './edit/edit.module#EditPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page {
+  public items: string;
+  list: {
+    content: string,
+    date: number;
+  };
+
+  constructor(
+    private storage: Storage,
+  ) {}
+
+  async ionViewWillEnter() {
+    this.storage.get('items').then((items) => {
+      this.items = items ? items : [];
+    });
+  }
+
+  check(val: string): void {
+    this.storage.set('items', this.items).then(() => {
+    });
+  }
+
+  addMemo() {
+    this.storage.set('items', this.items).then(() => {
+    });
+  }
+
+  ionViewWillLeave() {
+    this.addMemo();
+  }
 
 }
