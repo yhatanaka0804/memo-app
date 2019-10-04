649304c2a4a64ba5ad5b3b939b1ef9d2dcef058e
yhatanaka0804 <yhatanaka0804@gmail.com>
2019-10-04 17:58:32 +0900

    feat:削除機能実装

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { NavController } from '@ionic/angular';
+import { NavController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -14,11 +14,41 @@ export class Tab2Page {
   constructor(
     public storage: Storage,
     public navCtrl: NavController,
+    public actionSheetController: ActionSheetController,
   ) {}
 
   async ionViewWillEnter() {
     this.storage.get('lists').then((list) => {
       this.lists = JSON.parse(list);
+    });
+  }
+
+  async delCheck(index: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: '削除確認',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.delMemo(index);
+          }
+        }, {
+          text: 'キャンセル',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+      ]
+    });
+    actionSheet.present();
+  }
+  delMemo(index: number) {
+    this.lists.splice(index, 1);
+    this.storage.set('lists', JSON.stringify(this.lists)).then(() => {
     });
   }
 }
